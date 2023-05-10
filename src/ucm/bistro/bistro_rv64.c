542c03330475c1f188cb45c8d13cb261bf21504b
Rob Bradford <rbradford@rivosinc.com>
2023-05-10 09:53:50 +0000

    UCS/ARCH/RV64: Add RISC-V support
    This patch is the derived from the work done by Chris Taylor and John
    Leidel (from this PR: https://github.com/openucx/ucx/pull/8246/commits)
    
    The ways in which this patch differs from that PR are:
    
    * Use __clear_cache() for i-cache updates with correct syscall fallback
    (need to pass 0 for all CPUs) (myself)
    * Use appropriate fence instructions for IO and CPU fences (myself)
    * Coding standard fixes (myself)
    * Removal of unused rv64/atomic.h (myself)
    * Header guard fixes (myself)
    * Removal of unused UCS_CPU_MODEL_RV64IMAFDC (myself)
    * Corrected patch address immediate calculation (Evan Green)
    * Handling of zero for clz (Evan Green)
    * Use generic time functions (Evan Green)
    
    Original-author: Chris Taylor <ctaylor@tactcomplabs.com>
    Original-author: John Leidel <john.leidel@gmail.com>
    Co-authored-by: Evan Green <evan@rivosinc.com>
    Signed-off-by: Rob Bradford <rbradford@rivosinc.com>
    Signed-off-by: Evan Green <evan@rivosinc.com>

---
+++ src/ucm/bistro/bistro_rv64.c
@@ -0,0 +1,119 @@
+/**
+ * Copyright (C) Tactical Computing Labs, LLC. 2022. ALL RIGHTS RESERVED.
+ *
+ * See file LICENSE for terms.
+ */
+
+#ifdef HAVE_CONFIG_H
+#  include "config.h"
+#endif
+
+#if defined(__riscv)
+
+#include <ucs/arch/cpu.h>
+#include <ucm/bistro/bistro.h>
+#include <ucm/bistro/bistro_int.h>
+#include <ucs/debug/assert.h>
+#include <ucs/sys/math.h>
+#include <ucm/util/sys.h>
+
+#include <assert.h>
+#include <dlfcn.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/mman.h>
+
+/* Registers numbers to use with the move immediate to register.
+ * The destination register is X31 (highest temporary).
+ * Register X28-X30 are used for block shifting and masking.
+ * Register X0 is always zero
+ */
+#define X31 31
+#define X30 30
+#define X0  0
+
+/**
+  * @brief JALR - Add 12 bit immediate to source register, save to destination
+  * register, jump and link from destination register
+  *
+  * @param[in] _reg  register number (0-31), @param[out] _reg register number
+  * (0-31), @param[imm] 12 bit immmediate value
+  */
+#define JALR(_regs, _regd, _imm) \
+    (((_imm) << 20) | ((_regs) << 15) | (0b000 << 12) | ((_regd) << 7) | (0x67))
+
+/**
+  * @brief ADDI - Add 12 bit immediate to source register, save to destination
+  * register
+  *
+  * @param[in] _reg  register number (0-31), @param[out] _reg register number
+  * (0-31), @param[imm] 12 bit immmediate value
+  */
+#define ADDI(_regs, _regd, _imm) \
+    (((_imm) << 20) | ((_regs) << 15) | (0b000 << 12) | ((_regd) << 7) | (0x13))
+#define ADD(_regs_a, _regs_b, _regd) \
+    ((_regs_b << 20) | (_regs_a << 15) | (0b000 << 12) | ((_regd) << 7) | \
+     (0x33))
+
+/**
+  * @brief LUI - load upper 20 bit immediate to destination register
+  *
+  * @param[in] _reg  register number (0-31), @param[out] _reg register number
+  * (0-31), @param[imm] 12 bit immmediate value
+  */
+#define LUI(_regd, _imm) (((_imm) << 12) | ((_regd) << 7) | (0x37))
+
+/**
+  * @brief SLLI - left-shift immediate number of bits in source register into
+  * destination register
+  *
+  * @param[in] _reg  register number (0-31), @param[out] _reg register number
+  * (0-31), @param[imm] 12 bit immmediate value
+  */
+#define SLLI(_regs, _regd, _imm) \
+    (((_imm) << 20) | ((_regs) << 15) | (0b001 << 12) | ((_regd) << 7) | (0x13))
+
+ucs_status_t ucm_bistro_patch(void *func_ptr, void *hook, const char *symbol,
+                              void **orig_func_p,
+                              ucm_bistro_restore_point_t **rp)
+{
+    ucs_status_t status;
+    uintptr_t hookp = (uintptr_t)hook;
+    ucm_bistro_patch_t patch;
+
+    /*
+     * Account for the fact that JALR, ADD, and ADDI sign extend and may result
+     * in subtractions by adding extra to compensate.
+     */
+    hookp += ((hookp >> 11) & 0x1) << 12;
+    hookp += ((hookp >> 31) & 0x1) << 32;
+    hookp += ((hookp >> (32 + 11)) & 0x1) << (32 + 12);
+    patch  = (ucm_bistro_patch_t){
+        .rega = LUI(X31, hookp >> (32 + 12)),
+        .regb = ADDI(X31, X31, ((hookp >> 32) & 0xFFF)),
+        .regc = LUI(X30, hookp >> 12),
+        .regd = SLLI(X31, X31, 32),
+        .rege = ADD(X30, X31, X31),
+        .regf = JALR(X31, X0, (hookp & 0xFFF))
+    };
+
+    if (orig_func_p != NULL) {
+        return UCS_ERR_UNSUPPORTED;
+    }
+
+    status = ucm_bistro_create_restore_point(func_ptr, sizeof(patch), rp);
+    if (UCS_STATUS_IS_ERR(status)) {
+        return status;
+    }
+
+    return ucm_bistro_apply_patch(func_ptr, &patch, sizeof(patch));
+}
+
+ucs_status_t ucm_bistro_relocate_one(ucm_bistro_relocate_context_t *ctx)
+{
+    return UCS_ERR_UNSUPPORTED;
+}
+
+#endif
